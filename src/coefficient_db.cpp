a684eadf9146cd10930414e1c8217fd386b50382
linjoehan <linjoehan@yahoo.com>
2021-06-14 11:13:24 +0200

    Added a fast learning main. Runs learning on it's own game simulator, not requiring the actual game to play. May be less accurate that the normal learning, but faster.

--- src/coefficient_db.cpp
+++ src/coefficient_db.cpp
@@ -3,7 +3,8 @@
 Coefficient_db::Coefficient_db()
 :
   coefficients_size(11),
-  learning_population(10)
+  learning_population(10),
+  learning_fast_population(100)
 {
     
 }
@@ -144,6 +145,86 @@ void Coefficient_db::write_learning(std::vector<std::vector<double>> coefficient
     
     learning_file.close();
 }
+
+
+std::vector<std::vector<double>> Coefficient_db::get_learning_fast()
+{
+    std::vector<std::vector<double>> res;
+    std::fstream learning_file;
+    learning_file.open("../coefficient_db/learning_fast.txt",std::ios::in);
+    
+    if(learning_file.is_open())
+    {
+        std::string line;
+        while(learning_file.good() and res.size() < learning_fast_population and getline(learning_file,line))
+        {
+            std::vector<double> res_part;
+            std::vector<std::string> input_string = string_split(line);
+            
+            //warnings for when size of inputs and expected are different
+            if(input_string.size() < coefficients_size)
+            {
+                std::cerr << "WARN: Coefficients vector received for line number "<< res.size()+1 << " is to small: received a vector of size " << input_string.size() << " expected " << coefficients_size << " filling to required size with ones" << std::endl;
+            }
+            if(input_string.size() > coefficients_size)
+            {
+                std::cerr << "WARN: Coefficients vector received for line number "<< res.size()+1 << " is to large: received a vector of size " << input_string.size() << " expected " << coefficients_size << " ignoring the extra values received" << std::endl;
+            }
+            
+            for(unsigned i = 0; i < std::min(input_string.size() ,coefficients_size ); i++)
+            {
+                res_part.push_back( std::stod(input_string[i]));
+            }
+            
+            while(res_part.size() < coefficients_size)
+            {
+                res_part.push_back(1);
+            }
+            
+            res.push_back(res_part);
+        }
+    }
+    
+    while(res.size() < learning_fast_population)
+    {
+        //push ones to fill to required values
+        std::vector<double> res_part;
+        while(res_part.size() < coefficients_size)
+        {
+            res_part.push_back(1);
+        }
+        res.push_back(res_part);
+    }
+    
+    learning_file.close();
+    
+    return res;
+}
+
+void Coefficient_db::write_learning_fast(std::vector<std::vector<double>> coefficient_value_table)
+{
+    std::fstream learning_file;
+    learning_file.open("../coefficient_db/learning_fast.txt",std::ios::out);
+    
+    if(learning_file.is_open())
+    {
+        for(int row = 0;row<coefficient_value_table.size();row++)
+        {
+            for(int col = 0;col < coefficient_value_table[row].size();col++)
+            {
+                learning_file << (col>0? " ":"") << std::fixed << std::setprecision(15) << coefficient_value_table[row][col];
+            }
+            learning_file << std::endl;
+        }
+    }
+    else
+    {
+        std::cerr << "WARN: Could not open learning file for writing, skipping write" << std::endl; 
+    }
+    
+    learning_file.close();
+}
+
 
 std::vector<std::string> Coefficient_db::string_split(const std::string& str) 
 {
--- src/coefficient_db.h
+++ src/coefficient_db.h
@@ -13,11 +13,15 @@ class Coefficient_db
     Coefficient_db();
     const unsigned coefficients_size;
     const unsigned learning_population;
+    const unsigned learning_fast_population;
     
     std::vector<double> get_default();
     void write_default(std::vector<double> coefficient_value);
     std::vector<std::vector<double>> get_learning();
     void write_learning(std::vector<std::vector<double>> coefficient_value_table);
+    
+    std::vector<std::vector<double>> get_learning_fast();
+    void write_learning_fast(std::vector<std::vector<double>> coefficient_value_table);
     
     private:
     std::vector<std::string> string_split(const std::string& str);
